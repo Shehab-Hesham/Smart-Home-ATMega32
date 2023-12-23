44f041df6b8543f68ab1e8aecc49dc4ff32dab60
Shehab Hesham <145757675+Shehab-Hesham@users.noreply.github.com>
2023-12-23 22:00:04 +0200

    UART Configuration

---
+++ Drivers/MCAL/UART/UART_CINFIG.h
@@ -0,0 +1,45 @@
+
+#ifndef UART_UART_TYPES_H_
+#define UART_UART_TYPES_H_
+
+#include "..\..\STD.h"
+
+#define	UART_5BIT_MODE 		5
+#define	UART_6BIT_MODE 		6
+#define	UART_7BIT_MODE 		7
+#define	UART_8BIT_MODE 		8
+#define	UART_9BIT_MODE 		9
+
+#define UART_NUM_BIT_MODE   UART_8BIT_MODE
+
+#define	UART_STOP_1BIT  1
+#define	UART_STOP_2BIT  2
+
+#define UART_NUM_STOP_BIT  UART_STOP_1BIT
+
+#define	UART_PARITY_DISABLED 0
+#define	UART_PARITY_EVEN     1
+#define	UART_PARITY_ODD      2
+
+#define UART_PARITY_MODE    UART_PARITY_DISABLED
+
+//Buad for 16M
+#define  UART_BAUD_2400  416
+#define  UART_BAUD_4800  207
+#define  UART_BAUD_9600  103
+#define  UART_BAUD_14400  68
+#define  UART_BAUD_19200  51
+#define  UART_BAUD_28800  34
+#define  UART_BAUD_38400  25
+#define  UART_BAUD_57600  16
+#define  UART_BAUD_76800  12
+#define  UART_BAUD_115200  8
+#define  UART_BAUD_230400  3
+#define  UART_BAUD_250000  3
+#define  UART_BAUD_500000  1
+#define  UART_BAUD_1000000  0
+
+#define UART_BAUD_RATE  UART_BAUD_9600
+
+
+#endif /* UART_UART_TYPES_H_ */
