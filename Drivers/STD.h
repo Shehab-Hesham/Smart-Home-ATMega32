475ecb40ccdbc8260be8dcf669c20089a4a70c81
Shehab Hesham <145757675+Shehab-Hesham@users.noreply.github.com>
2023-12-23 21:32:11 +0200

    Datatypes Configuration

---
+++ Drivers/STD.h
@@ -0,0 +1,15 @@
+#ifndef STD_h
+#define STD_h
+
+
+typedef unsigned char uint8 ;
+typedef char int8 ;
+typedef unsigned short uint16 ;
+typedef short int16 ;
+typedef unsigned int uint32 ;
+typedef int int32 ;
+typedef double db ; 
+typedef float fl  ; 
+
+
+#endif // STD.h
